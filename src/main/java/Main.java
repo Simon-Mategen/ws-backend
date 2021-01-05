ddce070bb4e6739b1950557ac34749d0e7d09500
itsamelaurio <mslaurabarba@gmail.com>
2021-01-05 09:17:05 +0100

    CORS löst

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -189,6 +189,28 @@ public class Main
         Main prog = new Main();
         Gson gson = new Gson();
 
+        options("/*",
+                (request, response) -> {
+
+                    String accessControlRequestHeaders = request
+                            .headers("Access-Control-Request-Headers");
+                    if (accessControlRequestHeaders != null) {
+                        response.header("Access-Control-Allow-Headers",
+                                accessControlRequestHeaders);
+                    }
+
+                    String accessControlRequestMethod = request
+                            .headers("Access-Control-Request-Method");
+                    if (accessControlRequestMethod != null) {
+                        response.header("Access-Control-Allow-Methods",
+                                accessControlRequestMethod);
+                    }
+
+                    return "OK";
+                });
+
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+
         get("/api/v1/ledamoter", ((request, response) ->
         {
 
